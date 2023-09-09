d6d1625b313d314a9b019a91741dc1fe374c2e4f
Eduardo Soares <edurbs@gmail.com>
2023-09-09 17:11:30 -0300

    feat: boleto tests

--- src/test/java/br/com/medeirosecia/analyzemail/domain/service/email/AnalyzePDFTest.java
+++ src/test/java/br/com/medeirosecia/analyzemail/domain/service/email/AnalyzePDFTest.java
@@ -6,75 +6,34 @@ import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class AnalyzePDFTest {
-    @Test
-    void testGetBoletoSicredi() {
-        AnalyzePDF analyzePDF = new AnalyzePDF(this.getPath("boletoSicredi.pdf"));
-        String finalDate = this.getFinalDate(analyzePDF.getBoletoDate());
-        Assertions.assertEquals("11/09/2023", finalDate);
-        Assertions.assertTrue(analyzePDF.isBoleto());
-    }
 
-    @Test
-    void testGetBoletoSicredi2() {
-        AnalyzePDF analyzePDF = new AnalyzePDF(this.getPath("boletoSicredi2.pdf"));
-        String finalDate = this.getFinalDate(analyzePDF.getBoletoDate());
-        Assertions.assertEquals("10/10/2023", finalDate);
+    @ParameterizedTest
+    @CsvSource({ "boletoSicredi.pdf, 11/09/2023", 
+            "boletoSicredi2.pdf, 10/10/2023", 
+            "boletoVivo.pdf, 25/09/2023", 
+            "boletoSafra.pdf, 21/08/2023", 
+            "boletoBB2.pdf, 10/09/2023",
+            "boletoBB.pdf, 10/09/2023",
+            "guiaFunrural.pdf, 20/09/2023"
+        })
+    void testBoletosAndDate(String fileName, String date) {
+        AnalyzePDF analyzePDF = new AnalyzePDF(this.getPath(fileName));
+        Assertions.assertEquals(date, this.getFinalDate(analyzePDF.getBoletoDate()));
         Assertions.assertTrue(analyzePDF.isBoleto());
     }
-
-    @Test
-    void testGetBoletoVivo() {
-        AnalyzePDF analyzePDF = new AnalyzePDF(this.getPath("boletoVivo.pdf"));
-        String finalDate = this.getFinalDate(analyzePDF.getBoletoDate());
-        Assertions.assertEquals("25/09/2023", finalDate);
-        Assertions.assertTrue(analyzePDF.isBoleto());
-    }
-
-    @Test
-    public void testGetBoletoSafra(){        
-        AnalyzePDF analyzePDF = new AnalyzePDF(this.getPath("boletoSafra.pdf"));
-        String finalDate = this.getFinalDate(analyzePDF.getBoletoDate());
-        Assertions.assertEquals("21/08/2023", finalDate);
-        Assertions.assertTrue(analyzePDF.isBoleto());
-    }
-
-    @Test
-    public void testGetBoletoBB2(){
-        AnalyzePDF analyzePDF = new AnalyzePDF(this.getPath("boletoBB2.pdf"));
-        String finalDate = this.getFinalDate(analyzePDF.getBoletoDate());
-        Assertions.assertEquals("10/09/2023", finalDate);
-        Assertions.assertTrue(analyzePDF.isBoleto());
-    }
-
-    @Test
-    public void testGetBoletoBB(){        
-        AnalyzePDF analyzePDF = new AnalyzePDF(this.getPath("boletoBB.pdf"));
-        String finalDate = this.getFinalDate(analyzePDF.getBoletoDate());
-        Assertions.assertEquals("10/09/2023", finalDate);
-        Assertions.assertTrue(analyzePDF.isBoleto());
-    }
+        
+    
 
     @Test
     public void testOCRCheckIfIsNFPdfWithOnlyImage(){
         var analyzePDF = new AnalyzePDF(this.getPath("nfSoImagemNFSCuiaba.pdf"));        
         Assertions.assertTrue(analyzePDF.isNF());
     }
-    
-
-    @Test
-    public void testIfIsBoletoGuiaFunrural(){        
-        AnalyzePDF analyzePDF = new AnalyzePDF(this.getPath("guiaFunrural.pdf"));
-        Assertions.assertTrue(analyzePDF.isBoleto());
-    }
-
-    @Test
-    public void testGuiaFunrural(){        
-        AnalyzePDF analyzePDF = new AnalyzePDF(this.getPath("guiaFunrural.pdf"));
-        String finalDate = this.getFinalDate(analyzePDF.getBoletoDate());
-        Assertions.assertEquals("20/09/2023", finalDate);
-    }
+ 
 
     private String getFinalDate(String[] date){
         String finalDate = date[0]+"/"+date[1]+"/"+date[2];
