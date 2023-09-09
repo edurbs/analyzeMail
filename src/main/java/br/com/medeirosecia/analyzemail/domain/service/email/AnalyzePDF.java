e983513644acad4688a2fcd1df1075740415f845
Eduardo Soares <edurbs@gmail.com>
2023-09-09 16:59:06 -0300

    feat: OCR

--- src/main/java/br/com/medeirosecia/analyzemail/domain/service/email/AnalyzePDF.java
+++ src/main/java/br/com/medeirosecia/analyzemail/domain/service/email/AnalyzePDF.java
@@ -1,16 +1,24 @@
 package br.com.medeirosecia.analyzemail.domain.service.email;
 
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.input.CloseShieldInputStream;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.pdfbox.text.PDFTextStripper;
 
 import br.com.medeirosecia.analyzemail.domain.repository.Attachment;
 import net.rationalminds.LocalDateModel;
 import net.rationalminds.Parser;
+import net.sourceforge.tess4j.Tesseract;
 
 public class AnalyzePDF {
     private PDDocument pdfDocument;
@@ -24,20 +32,42 @@ public class AnalyzePDF {
     private String[] boletoKeywords = { "vencimento", "cedente", "referência", "pagador", "beneficiário",
             "nosso número", "valor do documento", "data do processamento", "mora", "multa", "juros", "carteira",
             "título", "pagável", "sacado", "débito automático", "total a pagar", "mês de referência", "serviços contratados",
-            "autenticação mecânica"
+            "autenticação mecânica", "período de apuração", "número do documento", "pagar este documento até",
+            "documento de arrecadação", "pagar até", "pague com o pix"
          };
 
     public AnalyzePDF(Attachment attachment) {
         try {
             ByteArrayInputStream inputStream = new ByteArrayInputStream(attachment.getData());
-            this.pdfDocument = PDDocument.load(CloseShieldInputStream.wrap(inputStream));
-            this.checkKeyWords();
-            this.pdfDocument.close();
-        } catch (IOException e) {
+            this.processPDF(inputStream);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     
     }
+
+    public AnalyzePDF(String filePath){
+        try {
+            FileInputStream fis = new FileInputStream(filePath);            
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(IOUtils.toByteArray(fis));
+            this.processPDF(inputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void processPDF(ByteArrayInputStream inputStream){
+        try {
+            this.pdfDocument = PDDocument.load(CloseShieldInputStream.wrap(inputStream));
+            this.checkKeyWords();
+            this.pdfDocument.close();
+        } catch (IOException e) {            
+            e.printStackTrace();
+        }
+    }
+
+    
 
     
     public boolean isNF(){
@@ -53,10 +83,6 @@ public class AnalyzePDF {
         }        
         return false;
     }
-    
-    public AnalyzePDF(){
-        
-    }
 
     public String[] getBoletoDate(){        
         Parser parser = new Parser();
@@ -65,7 +91,24 @@ public class AnalyzePDF {
         
         List<LocalDateModel> dates = parser.parse(text);
 
-        var date = dates.get(0).getOriginalText();
+        String date="00/00/0000";   
+
+        // search for a date with /
+        for (LocalDateModel localDateModel : dates) {
+            if (localDateModel.getOriginalText().contains("/")) {
+                
+                // check if date is of this year, next year or last year
+                int year = Integer.parseInt(localDateModel.getOriginalText().split("/")[2]);
+                int thisYear = java.time.LocalDate.now().getYear();
+                if (year == thisYear + 1
+                        || year == thisYear - 1
+                        || year == thisYear) {
+                    date = localDateModel.getOriginalText();
+                    return date.split("/");
+                }
+
+            }
+        }
 
         return date.split("/");
 
@@ -74,12 +117,33 @@ public class AnalyzePDF {
     public void setPdfText(String pdfText) {
         this.pdfText = pdfText.toLowerCase();
     }
-     
+
+    public String getPdfText() {
+        return pdfText;
+    }
 
     private void checkKeyWords() {
         try {
             PDFTextStripper pdfTextStripper = new PDFTextStripper();
-            this.pdfText = pdfTextStripper.getText(pdfDocument).toLowerCase();
+            this.pdfText = pdfTextStripper.getText(this.pdfDocument).toLowerCase();
+
+            // if PDF has no text, works with OCR
+            if(this.pdfText.length()<10){
+                PDFRenderer pdfRenderer = new PDFRenderer(pdfDocument);
+                BufferedImage bufferedImage = pdfRenderer.renderImageWithDPI(0, 300, ImageType.GRAY);
+
+                Tesseract tesseract = new Tesseract();
+
+                URL url = AnalyzePDF.class.getResource("/tesseract/");
+                String tessractDataPath = Paths.get(url.toURI()).toString();
+                tesseract.setDatapath(tessractDataPath);
+
+                tesseract.setLanguage("por");
+                tesseract.setPageSegMode(1); // Automatic Page Segmentation with OSD
+                
+                String result = tesseract.doOCR(bufferedImage);
+                this.pdfText = result.toLowerCase();
+            }
             
             for (String keyword : nfKeywords) {
                 if (pdfText.contains(keyword)) {
@@ -94,9 +158,9 @@ public class AnalyzePDF {
                 }
             }
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
+        } 
 
     }
 
