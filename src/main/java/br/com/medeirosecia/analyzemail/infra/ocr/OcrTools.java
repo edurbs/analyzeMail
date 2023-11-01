acb056ea0d54d19806b5cb4eafad4691cbfc63e7
Eduardo Soares <edurbs@gmail.com>
2023-11-01 18:11:11 -0300

    fix: ocr

--- src/main/java/br/com/medeirosecia/analyzemail/infra/ocr/OcrTools.java
+++ src/main/java/br/com/medeirosecia/analyzemail/infra/ocr/OcrTools.java
@@ -8,21 +8,23 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
 
-import br.com.medeirosecia.analyzemail.domain.service.email.HandlePdf;
 import net.sourceforge.tess4j.Tesseract;
 
 public class OcrTools {
     private PDDocument pdfDocument;
-    public OcrTools(PDDocument pdfDocument){
+
+    public OcrTools(PDDocument pdfDocument) {
         this.pdfDocument = pdfDocument;
     }
 
-
-    private String getTextFromImage(BufferedImage bufferedImage){
+    private String getTextFromImage(BufferedImage bufferedImage) {
         try {
             Tesseract tesseract = new Tesseract();
-            URL url = HandlePdf.class.getResource("/tesseract/fast/");
+
+            URL url = new URL("file:///C:/Program%20Files/Tesseract-OCR");
+
             String tessractDataPath = Paths.get(url.toURI()).toString();
+
             tesseract.setDatapath(tessractDataPath);
             tesseract.setLanguage("por");
             tesseract.setPageSegMode(1); // Automatic Page Segmentation with OSD
@@ -35,26 +37,17 @@ public class OcrTools {
         return "";
     }
 
-    public String getTextFromPage(int page){
-        if (page>pdfDocument.getNumberOfPages()){
+    public String getTextFromPage(int page) {
+        if (page > pdfDocument.getNumberOfPages()) {
             return "";
         }
 
         try {
             PDFRenderer pdfRenderer = new PDFRenderer(pdfDocument);
-            //int numPages = pdfDocument.getNumberOfPages();
 
-            //StringBuilder resultBuilder = new StringBuilder();
+            BufferedImage bufferedImage = pdfRenderer.renderImageWithDPI(page - 1, 300, ImageType.GRAY);
+            return getTextFromImage(bufferedImage);
 
-
-            // for (int page = 0; page < numPages; page++) {
-                BufferedImage bufferedImage = pdfRenderer.renderImageWithDPI(page-1, 300, ImageType.GRAY);
-                return getTextFromImage(bufferedImage);
-
-                // resultBuilder.append(pageResult);
-            // }
-
-            // return resultBuilder.toString();
         } catch (Exception e) {
             e.printStackTrace();
         }
